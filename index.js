961247a637eab7d5057d88ba692dd7cc52ce2570
rohit <rohitchugh142001@gmail.com>
2023-08-31 04:45:27 +0530

    Added a controller and a router for home

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require(`express`);
 const port = 8900;
 const app = express();
 
+// use express router 
+app.use('/',require('./routes'));
+
+
+//running the server
 app.listen(port, (err)=>{
     if(err){
         console.log(`error in running the server: ${err}`); // this is called interpolation
