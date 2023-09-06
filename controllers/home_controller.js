9ecff16c1bbb6c9c6597fbc480e3cf31476f2fd6
rohit <rohitchugh142001@gmail.com>
2023-09-06 16:33:58 +0530

    displayed all posts on hme page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,18 +1,17 @@
 const Post = require('../models/posts');
 const User = require('../models/users')
-module.exports.home = function(req,res){
+module.exports.home = async function(req,res){
     // return res.end(`<h1> Express is up for Codial!!</h1>`);
+
+    // populate the user of each post
+    const posts = await Post.find({}).populate('user');
     
     return res.render('home',{
-        title: 'Home'
+        title: 'Home',
+        posts: posts
     });
 }
 
-module.exports.createPost = async (req,res)=>{
-    await Post.create({
-        content: req.body.content
-    });
-    return res.redirect('back');
-}
+
 
 // module.exports.actionName = function(req,res){}
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const homeController = require("../controllers/home_controller");
 const passport = require('passport');
 console.log('router loaded');
 
-router.post('/',passport.checkAuthentication,homeController.createPost);
+// router.post('/',passport.checkAuthentication,homeController.createPost);
 router.get('/',homeController.home);
 router.use('/posts',require('./posts'));
 router.use('/users',require('./users'));
