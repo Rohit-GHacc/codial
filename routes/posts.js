7893c9833469334770fd481fb1301b20d49e6d5e
rohit <rohitchugh142001@gmail.com>
2023-09-06 16:41:32 +0530

    checked authentication and then allowed to post

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport')
 const posts_controller = require('../controllers/posts_controller')
-router.post('/create',posts_controller.createPost);
+router.post('/create',passport.checkAuthentication,posts_controller.createPost);
 module.exports = router;
