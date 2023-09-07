1cb6ea0c2153ef0f458fba3623100e6427113f47
rohit <rohitchugh142001@gmail.com>
2023-09-07 14:19:46 +0530

    Deleting posts done

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,8 +1,29 @@
 const Post = require('../models/posts')
+const Comment = require('../models/comments')
 module.exports.createPost = async (req,res)=>{
     await Post.create({
         content: req.body.content,
         user: req.user._id
     });
     return res.redirect('back');
+}
+
+module.exports.destroy = async function(req,res){
+    try{
+        console.log(req.params.id)
+        const post = await Post.findById(req.params.id);
+        console.log(post.user ,'\n', req.user.id)
+    if(post.user == req.user.id){
+        post.deleteOne();
+
+        await Comment.deleteMany({post: req.params.id})
+        res.redirect('back');
+    }
+    else{
+        res.redirect('back');
+    }
+    }
+    catch(err){
+        console.log(err);
+    }
 }
--- routes/posts.js
+++ routes/posts.js
@@ -3,4 +3,5 @@ const router = express.Router();
 const passport = require('passport')
 const posts_controller = require('../controllers/posts_controller')
 router.post('/create',passport.checkAuthentication,posts_controller.createPost);
+router.get('/destroy/:id',passport.checkAuthentication,posts_controller.destroy);
 module.exports = router;
