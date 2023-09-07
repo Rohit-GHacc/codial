4eb38ebe3ab085330e22bd1c0c00653c977df4a3
rohit <rohitchugh142001@gmail.com>
2023-09-07 14:58:50 +0530

    Deleting comments done

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -21,3 +21,23 @@ module.exports.createComment = async function(req,res){
     } 
     
 }
+
+module.exports.destroy = async function(req,res){
+    try{
+        const comment = await Comment.findById(req.params.id);
+        if(comment.user == req.user.id){
+            
+            let postId = comment.post; 
+            comment.deleteOne();
+
+            await Comment.findByIdAndUpdate(postId,{ $pull: {comments: req.params.id}})
+            return res.redirect('back');
+        }
+        else{
+            return res.redirect('back');
+        }
+    }
+    catch(err){
+        console.log(err);
+    }
+}
--- routes/comments.js
+++ routes/comments.js
@@ -3,4 +3,5 @@ const router = express.Router();
 const passport = require('passport');
 const comments_controller = require('../controllers/comments_controller');
 router.post('/create',passport.checkAuthentication,comments_controller.createComment);
+router.get('/destroy/:id',passport.checkAuthentication,comments_controller.destroy);
 module.exports = router;
